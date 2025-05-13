b73bae2d083b58630a71261cf68bda34bd023132
kotyba-km <kotybaalkassarkm@gmail.com>
2025-05-13 08:54:20 +0200

    syria hurra

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+import Home from "./pages/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <Navbar />
-      <Hero />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
