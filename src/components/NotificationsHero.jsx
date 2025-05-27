0297ab2326174310b5e35e4a71576221ab20882f
kotyba-km <kotybaalkassarkm@gmail.com>
2025-05-27 11:06:38 +0200

    notifications hero page V-0.1

--- src/components/NotificationsHero.jsx
+++ src/components/NotificationsHero.jsx
@@ -1,5 +1,11 @@
 import styles from "./notificationsHero.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 export default function NotificationsHero() {
-  return <div></div>;
+  return (
+    <div className={`w-100 ${styles.bgSmoke}`}>
+      <h1 className={`w-50 m-auto text-center ${styles.titleHero}`}>
+        الإشعارات
+      </h1>
+    </div>
+  );
 }
--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import NotificationsHero from "../components/NotificationsHero";
 
 export default function Notifications() {
   return (
     <div>
       <Navbar />
+      <NotificationsHero />
     </div>
   );
 }
